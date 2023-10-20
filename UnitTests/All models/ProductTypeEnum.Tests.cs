c4930d183edd2862dc3be39c0f544029841a06a9
Alfin <alfinabraham28@gmail.com>
2023-10-20 15:57:18 -0700

    Added unit testing for ProductTypeEnum for all category options.

---
+++ UnitTests/All models/ProductTypeEnum.Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using YourMobileGuide.Models;
+
+namespace UnitTests.Models
+{
+    // Unit testing of ProductTypeEnum.cs
+    public class ProductTypeEnumTests
+    {
+        #region TestSetup
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        #region StaticMethodsTests
+        [Test]
+        [TestCase(2020)]
+        [TestCase(2021)]
+        [TestCase(2022)]
+        [TestCase(2023)]
+        public void Valid_Enum_Should_Return_Correct_Name(int year)
+        {
+            // Arrange
+            ProductTypeEnum enumTest = (ProductTypeEnum)Enum.Parse(typeof(ProductTypeEnum), "Year" + year);
+
+            // Act
+            var result = enumTest.GetName();
+
+            // Assert
+            Assert.AreEqual(true, result.Equals(year.ToString()));
+        }
+
+        #endregion StaticMethodTests
+    }
+}
