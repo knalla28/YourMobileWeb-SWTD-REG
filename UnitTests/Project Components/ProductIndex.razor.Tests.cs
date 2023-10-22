c6560f854133ec04772333ade8e6ea4f55f9c671
Karthik Nalla <knalla@seattleu.edu>
2023-10-22 15:38:37 -0700

    Added unit test for product index file

---
+++ UnitTests/Project Components/ProductIndex.razor.Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using YourMobileGuide.Components;
+using YourMobileGuide.Services;
+
+namespace UnitTests.Components
+{
+    // Unit tests for Product Index
+    public class ProductIndexTests : BunitTestContext
+    {
+        #region TestSetup
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+        #endregion
+
+        #region ProductIndex_Default_Should_Return_Content()
+        [Test]
+        public void ProductIndex_Default_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act
+            var page = RenderComponent<ProductIndex>();
+            var result = page.Markup;
+
+            // Assert
+            Assert.AreEqual(true, result.Contains("iphone_12"));
+        }
+        #endregion
+    }
+}
